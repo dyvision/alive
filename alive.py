a9a6d72cac9250a7638b742b0205e7d8a33f7ae2
Dylan Lang <34869448+dyvision@users.noreply.github.com>
2025-04-29 13:46:23 -0400

    mac support

--- alive.py
+++ alive.py
@@ -1,9 +1,11 @@
 #I'm totally working 2.0
-from pynput.keyboard import Key, Controller
+from pynput import keyboard,mouse
 import time
 import os
 import datetime
 import screeninfo as sc
+
+WAIT = 30
 
 def clear():
     if os.name == 'nt':  # For Windows
@@ -11,8 +13,27 @@ def clear():
     else:  # For MacOS and Linux
         os.system('clear')
 
+def check():
+    if os.name == 'nt':  # For Windows
+        return 0
+    else:  # For MacOS and Linux
+        return 1
+
+def click(sys_type):
+    if sys_type == 0:  
+        keys = keyboard.Controller()
+        keys.press(keyboard.Key.f19)
+        keys.release(keyboard.Key.f19)
+    else:
+        moose = mouse.Controller()
+        moose.press(mouse.Button.right)
+        moose.release(mouse.Button.right)
+
+
+
 start = datetime.datetime.now()
 i = 0
+sys_type = check()
 
 while 0 is not 1:
     for m in sc.get_monitors():
@@ -23,10 +44,7 @@ while 0 is not 1:
     clear()
     print('Started: {}'.format(start))
     print('Iteration: {}'.format(i))
-    mouse = Controller()
-    mouse.press(Key.f19)
-    mouse.release(Key.f19)
     print('Resolution: X {} | Y {}'.format(x,y))
-    mouse = ''
+    click(sys_type)
     i = i+1
-    time.sleep(30)
+    time.sleep(WAIT)
